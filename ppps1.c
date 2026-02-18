85cec4abc36c2f5e9715f15f1029836b121d8f6c
puurviisiingh <puurviisiingh@gmail.com>
2026-02-18 14:08:09 +0530

    Add files via upload

---
+++ ppps1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() 
+{
+
+    float hindi,physics,maths,chemistry,sum,percentage;
+
+    printf("enter number in hindi :");
+    scanf("%f", &hindi);
+
+    printf("enter number in physics :");
+    scanf("%f", &physics);
+    
+    printf("enter number in maths :");
+    scanf("%f", &maths);
+
+    printf("enter number in chemistry :");
+    scanf("f", &chemistry);
+
+    sum = hindi + physics + maths + chemistry;
+    printf("sum %f\n",sum);
+    printf("percentage = %.2f%%\n", percentage);
+    return 0;
+}
